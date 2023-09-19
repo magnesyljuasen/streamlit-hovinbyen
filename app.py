7b4f594b58f261b06d0dd0cab7edc835e5173cd0
magnesyljuasen <msylju@gmail.com>
2023-09-19 11:03:00 +0200

    test ny layout

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def select_scenario(df):
     options = df.columns
     default_options = options.to_list()
 
-    selected_scenarios = st.multiselect("Velg scenarier", options = options, default = default_options)
+    selected_scenarios = st.multiselect("Velg scenarier", options = options, default = default_options, help = "Her kan du velge ett eller")
     if len(selected_scenarios) == 0:
         st.stop()
     return df[selected_scenarios]
@@ -37,28 +37,56 @@ def sort_columns_high_to_low(df):
 def rounding_to_int(number):
     return int(round(number, 0))
 
-def plot_dataframe(df, sorting = True):
+def plot_dataframe(df, color_sequence, sorting = True):
     if sorting == True:
         df = sort_columns_high_to_low(df)
-    fig = px.line(df, x=df.index, y=df.columns)
-    fig["data"][0]["showlegend"] = True
-    fig.update_layout(
-        legend={'title_text':''},
-        barmode="stack", margin=dict(l=20, r=20, t=20, b=20),
-        plot_bgcolor="white", paper_bgcolor="white",
-        legend_traceorder="reversed"
+        fig = px.line(df, x=df.index, y=df.columns, color_discrete_sequence=color_sequence)
+        fig.update_layout(legend=dict(bgcolor="rgba(0,0,0,0)"))
+        
+        fig.update_xaxes(
+            range=[0, 8760],
+            title_text='Varighet [timer]',
+            mirror=True,
+            ticks="outside",
+            showline=True,
+            linecolor="black",
+            gridcolor="lightgrey",
+            )
+    else:
+        fig = px.area(df, x=df.index, y=df.columns)
+        fig.update_traces(
+            line=dict(
+                width=0, 
+                color = color_sequence
+            ))
+        fig.update_layout(
+            legend=dict(yanchor="top", y=0.98, xanchor="left", x=0.01, bgcolor="rgba(0,0,0,0)"),
+            xaxis = dict(
+                tickmode = 'array',
+                tickvals = [0, 24 * (31), 24 * (31 + 28), 24 * (31 + 28 + 31), 24 * (31 + 28 + 31 + 30), 24 * (31 + 28 + 31 + 30 + 31), 24 * (31 + 28 + 31 + 30 + 31 + 30), 24 * (31 + 28 + 31 + 30 + 31 + 30 + 31), 24 * (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31), 24 * (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30), 24 * (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31), 24 * (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30), 24 * (31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31)],
+                ticktext = ["1.jan", "", "1.mar", "", "1.mai", "", "1.jul", "", "1.sep", "", "1.nov", "", "1.jan"]
+                )
         )
-    fig.update_xaxes(
+        fig.update_xaxes(
         range=[0, 8760],
-        title_text='Varighet [timer]',
+        title_text='',
         mirror=True,
         ticks="outside",
         showline=True,
         linecolor="black",
         gridcolor="lightgrey",
         )
+    fig["data"][0]["showlegend"] = True
+    fig.update_layout(
+        legend={'title_text':''},
+        barmode="stack", margin=dict(l=20, r=20, t=20, b=20),
+        plot_bgcolor="white", paper_bgcolor="white",
+        legend_traceorder="reversed",
+        )
+    
     
     fig.update_yaxes(
+        range=[0, 70],
         title_text='Effekt [MW]',
         mirror=True,
         ticks="outside",
@@ -67,36 +95,83 @@ def plot_dataframe(df, sorting = True):
         gridcolor="lightgrey",
     )
     # Display the plot
-    st.plotly_chart(fig, use_container_width = True)
+    st.plotly_chart(fig, use_container_width = True, config = {'displayModeBar': False})
 
-def show_metrics(df):
+def show_metrics(df, color_sequence):
+    c1, c2 = st.columns(2)
     options = df.columns
+    reference_max = np.max(df["Referansesituasjon"])
+    reference_sum = np.sum(df["Referansesituasjon"]) / 1000
     for i in range(0, len(options)):
-        series = df[options[i]]
-        max_value = rounding_to_int(np.max(series))
-        sum_value = rounding_to_int(np.sum(series)/1000)
-        st.header(f"{df.columns[i]}")
-        column_1, column_2 = st.columns(2)
-        with column_1:
-            st.metric(f"Maksimal kjøpt effekt fra nettet", value = f"{max_value} MW")
-        with column_2:
-            st.metric(f"Kjøpt energi fra nettet", value = f"{sum_value} GWH/år")
-        st.markdown("---")
+        if (i % 2):
+            col = c1
+        else:
+            col = c2
+        with col:
+            series = df[options[i]]
+            max_value = rounding_to_int(np.max(series))
+            sum_value = rounding_to_int(np.sum(series)/1000)
+            max_value_reduction = int(((reference_max - max_value)/reference_max) * 100)
+            sum_value_reduction = int(((reference_sum - sum_value)/reference_sum) * 100)
+            with st.container():
+                st.header(f"{df.columns[i]}")
+                column_1, column_2 = st.columns(2)
+                delta_color_1 = "inverse"
+                delta_1 = f"{-max_value_reduction} %"
+                delta_color_2 = "inverse"
+                delta_2 = f"{-sum_value_reduction} %"
+                if max_value_reduction == 0:
+                    delta_color_1 = "off"
+                    delta_1 = "Ingen reduksjon"
+                if sum_value_reduction == 0:
+                    delta_color_2 = "off"
+                    delta_2 = "Ingen reduksjon"
+
+                with column_1:
+                    st.metric(f"Maksimal kjøpt effekt fra nettet", value = f"{max_value} MW", delta = delta_1, delta_color=delta_color_1)
+                with column_2:
+                    st.metric(f"Kjøpt energi fra nettet", value = f"{sum_value} GWH/år", delta = delta_2, delta_color=delta_color_2)
+                #--
+                #st.write(df)
+                df_option = df[df.columns[i]].to_frame()
+                plot_dataframe(df = df_option, color_sequence = color_sequence[i], sorting = False)
+                with st.expander("Se data"):
+                    st.write(df_option)
+                st.markdown("---")
 
 def main():
     st.set_page_config(
     page_title="Nedre Glomma",
-    page_icon="📈")
+    page_icon="📈",
+    layout="wide")
 
     with open("app.css") as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
-
-    st.title("Resultater Nedre Glomma")
+    st.title("Overordnede resultater")
     df = csv_to_df(folder_path = "data")
     df = select_scenario(df)
-    plot_dataframe(df, sorting = True)
-    #plot_dataframe(df, sorting = False)
-    show_metrics(df)
+    #color_sequence = px.colors.qualitative.Dark2
+    color_sequence = [
+    "#1d3c34",
+    "#48a23f",
+    "#FFC358",
+    "#ff5733",
+    "#33FF57",
+    "#5733FF",
+    "#FF33E6",
+    "#33E6FF",
+    "#FF5733",
+    "#FF33E6",
+    "#5733FF",
+    "#33E6FF",
+]
+
+
+    plot_dataframe(df = df, color_sequence = color_sequence, sorting = True)
+    with st.expander("Se data"):
+        st.write(df)
+    #--
+    show_metrics(df, color_sequence)
 
 if __name__ == '__main__':
     main()
